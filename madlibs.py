45906ac03a1e609d43e1966fc4f15ea13dffdfea
Vazgen.Apoyan <166911911+VazgenAP@users.noreply.github.com>
2024-04-17 22:21:07 +0400

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#youtuber = 'VazgenAP'
+# ways to make madlibs
+#print('subscribe to ' + youtuber)
+#print('subscribe to {}'.format(youtuber))
+#print(f"subscribe to {youtuber}") 
+
+adj = input('Adjective: ')
+verb_1 = input('Verb: ')
+verb_2 = input('Verb: ')
+famous_person = input('Famous person: ')
+
+madlib = f"Computer programming is so {adj}! It makes me excited all the time, because I love to {verb_1}. Stay hydrated and {verb_2} like you are {famous_person}!"
+
+print(madlib)
